dc79f420033c2fc0c9129a9368f502c391b54ec9
Chris Oliver <excid3@gmail.com>
2014-01-04 21:11:52 -0600

    Update deploy and production env to include mail

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :deploy_to, '/home/deploy/favorite'
 # set :log_level, :debug
 # set :pty, true
 
-set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_files, %w{config/database.yml config/application.yml config/mail.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
